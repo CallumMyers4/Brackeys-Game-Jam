34c086cb06156c658a0ae361dbc71249e52b4352
CallumMyers4 <callummyers4@gmail.com>
2025-01-27 21:00:08 +0000

    Added a UI to base scene
    Now shows wood collected from runs

--- Gather Dash/Assets/Scripts/Controllers/CentralBaseControllerScript.cs
+++ Gather Dash/Assets/Scripts/Controllers/CentralBaseControllerScript.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 
 public class CentralBaseControllerScript : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI woodDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        if (Input.anyKeyDown)
-        {
-            SceneManager.LoadScene("Forest Runner");
-        }
+        woodDisplay.text = "Wood:" + OverallGameManagerScript.globalManager.woodCollected;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Forest Runner");
+        }
     }
 }
